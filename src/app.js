9c8381ce33265e09188258b07b049b7ea15853b7
callmevaleina <valentina.crms@gmail.com>
2022-10-26 21:45:30 -0300

    changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ const usersRoutes = require('./routes/users.routes');
 
 const app = express();
 app.use(express.json());
-app.use('api/v1', usersRoutes);
+app.use('/api/v1', usersRoutes);
 
 module.exports = app;
